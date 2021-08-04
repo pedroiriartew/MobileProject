1fdcd0d48fbddfe940b099e9f97d52a8a45027e3
pedroiriartew <pedroiriartew@gmail.com>
2021-08-04 14:50:47 -0300

    it works!

---
+++ Assets/Scripts/PlayerScripts/JoystickInput.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickInput : MonoBehaviour
+{
+    [SerializeField] private RectTransform _stick;
+    [SerializeField] private RectTransform _stickArea;
+
+    private float _maxMovement;
+
+    private void Awake()
+    {
+        _maxMovement = _stickArea.sizeDelta.x / 2;
+    }
+
+    private void StickMovement()
+    {
+        _stick.transform.position = Input.mousePosition;
+
+        Vector3 stickDelta = _stick.position - _stickArea.position;
+
+        if (stickDelta.magnitude > _maxMovement)
+        {
+            _stick.position = _stickArea.position + stickDelta.normalized * _maxMovement;
+        }
+    }
+
+    public Vector3 GetAxises()
+    {
+        return (_stick.position - _stickArea.position) / _maxMovement;
+    }
+    public float GetAxisHorizontal()
+    {
+        return (_stick.position - _stickArea.position).x / _maxMovement;
+    }
+    public float GetAxisVertical()
+    {
+        return (_stick.position - _stickArea.position).y / _maxMovement;
+    }
+
+    public void OnMouseButtonUp()
+    {
+        _stick.localPosition = Vector3.zero;
+    }
+
+    public void OnMouseButtonDown()
+    {
+        StickMovement();
+    }
+}
--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -9,65 +9,27 @@ public class PlayerMovement : MonoBehaviour
     public event AnimPlayer RunAnimation;
 
     [SerializeField] private float _speed = 1.0f;
-    [SerializeField] private float _errorMargin = 20f;
 
-    [SerializeField] private Vector2 _horizontalGesture = new Vector2(100, 0);
-    private Vector2 _nullVector = new Vector2(-10000, -10000);
-    private Vector2 _startPosition;
+    [SerializeField] private JoystickInput _joystick;
 
-    private float _horizontalMovement;
+    [SerializeField] private Rigidbody2D _rb;
 
-    private Rigidbody2D _rb;
-
-    private void Awake()
-    {
-        _rb = gameObject.GetComponent<Rigidbody2D>();
-    }
+    private Vector2 _dir;
 
     private void Update()
     {
-        GetInput();
-    }
-
-    private void GetInput()
-    {
-        if (Input.touchCount > 0)
+        if (_joystick.GetAxisHorizontal() != 0)
         {
-            Touch touch = Input.GetTouch(0);
+            _dir = new Vector2(_joystick.GetAxisHorizontal(), 0);
 
-            if (touch.phase == TouchPhase.Began)
-            {
-                _startPosition = touch.position;
-            }
-            else if (touch.phase == TouchPhase.Moved)
-            {
-                MoveHero(touch);
-            }
-            else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
-            {
-                _startPosition = _nullVector;
-            }
-
-            RunAnimation?.Invoke(Mathf.Abs(_horizontalMovement), _horizontalMovement);//Absolute value to avoid "-1" case. The second value is for the Flip Method; "-1" means left.
+            RunAnimation?.Invoke(Mathf.Abs(_joystick.GetAxisHorizontal()), _joystick.GetAxisHorizontal());//Absolute value to avoid "-1" case. The second value is for the Flip Method; "-1" means left.
         }
     }
 
-    private void MoveHero(Touch touch)
+    private void FixedUpdate()
     {
-        Vector2 deltaMovement = touch.position - _startPosition;//Con esto sabemos cuanto se movió 
-
-        if (CheckGesture(_horizontalGesture, deltaMovement))
-        {
-            _rb.AddForce(Vector2.right * _horizontalMovement * _speed);
-        }
+        _rb.AddForce(_dir * _speed);
     }
-
-    private bool CheckGesture(Vector2 gesture, Vector2 deltaMovement)
-    {
-        return ((gesture - deltaMovement).magnitude <= _errorMargin);
-    }
-
-
 }
 
     //// Update is called once per frame
